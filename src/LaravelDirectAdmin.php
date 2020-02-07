a128b39106625374e31547b563c653f1f2d6fdf3
Topher L <topher@meanservers.com>
2020-02-07 21:26:23 +0100

    ignore camelcase if $string is SSL

--- src/LaravelDirectAdmin.php
+++ src/LaravelDirectAdmin.php
@@ -111,12 +111,17 @@ class LaravelDirectAdmin
      */
     private function camelToSnake($string)
     {
-        preg_match_all('!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!', $string, $matches);
-        $ret = $matches[0];
-        foreach ($ret as &$match) {
-            $match = $match == strtoupper($match) ? strtolower($match) : lcfirst($match);
+        if($string == "SSL"){
+            return "SSL";
+        }else {
+            preg_match_all('!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!', $string, $matches);
+            $ret = $matches[0];
+            foreach ($ret as &$match) {
+                $match = $match == strtoupper($match) ? strtolower($match) : lcfirst($match);
+            }
+
+            return strtoupper(implode('_', $ret));
         }
-        return strtoupper(implode('_', $ret));
     }
 
 }
