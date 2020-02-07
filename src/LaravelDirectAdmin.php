781dc5f53c511b031395772c4d36b07613ead51a
Topher L <topher@meanservers.com>
2020-02-07 21:36:51 +0100

    debug

--- src/LaravelDirectAdmin.php
+++ src/LaravelDirectAdmin.php
@@ -97,10 +97,18 @@ class LaravelDirectAdmin
     {
         $this->connection->set_method(strtoupper($method));
 
-        return $this->request(
-            $this->camelToSnake($command),
-            $arguments
-        );
+        dd($command);
+        if($command == "SSL"){
+            return $this->request(
+                "SSL",
+                $arguments
+            );
+        }else {
+            return $this->request(
+                $this->camelToSnake($command),
+                $arguments
+            );
+        }
     }
 
     /**
@@ -111,17 +119,12 @@ class LaravelDirectAdmin
      */
     private function camelToSnake($string)
     {
-        if($string == "SSL"){
-            return "SSL";
-        }else {
-            preg_match_all('!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!', $string, $matches);
-            $ret = $matches[0];
-            foreach ($ret as &$match) {
-                $match = $match == strtoupper($match) ? strtolower($match) : lcfirst($match);
-            }
-
-            return strtoupper(implode('_', $ret));
+        preg_match_all('!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!', $string, $matches);
+        $ret = $matches[0];
+        foreach ($ret as &$match) {
+            $match = $match == strtoupper($match) ? strtolower($match) : lcfirst($match);
         }
+        return strtoupper(implode('_', $ret));
     }
 
 }
